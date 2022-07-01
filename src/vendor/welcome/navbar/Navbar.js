cb709b691a1163f3e10cc7d052d94f90d6fb4554
Gaspard Wierzbinski <gaspard.wierzbinski@gmail.com>
2022-07-01 06:35:36 +0200

    Site is now mobile compatible (still looks cooler on pc tho)

--- src/vendor/welcome/navbar/Navbar.js
+++ src/vendor/welcome/navbar/Navbar.js
@@ -53,21 +53,21 @@ export default function Navbar() {
 				</nav>
 			</div>
 
-			{/*<Transition as={Fragment} enter="duration-150 ease-out" enterFrom="opacity-0 scale-95"
+			<Transition as={Fragment} enter="duration-150 ease-out" enterFrom="opacity-0 scale-95"
 				enterTo="opacity-100 scale-100" leave="duration-100 ease-in" leaveFrom="opacity-100 scale-100"
 				leaveTo="opacity-0 scale-95">
 				<Popover.Panel focus
 					className="absolute inset-x-0 top-0 p-2 transition origin-top transform md:hidden">
-					<div className="overflow-hidden bg-white rounded-lg shadow-md ring-1 ring-black ring-opacity-5">
+					<div className="overflow-hidden bg-gray-800 rounded-lg shadow-md ring-1 ring-black ring-opacity-5">
 						<div className="flex items-center justify-between px-5 pt-4">
 							<div>
-								<img className="w-auto h-8"
-									src="https://tailwindui.com/img/logos/workflow-mark-teal-500-cyan-600.svg"
+								<img className="w-8 w-auto h-8"
+									src="https://cdn.discordapp.com/icons/812825311002624060/c47013b226b3d61b65c817505b707a78.webp?size=96"
 									alt="" />
 							</div>
 							<div className="-mr-2">
 								<Popover.Button
-									className="inline-flex items-center justify-center p-2 text-gray-400 bg-white rounded-md hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-cyan-600">
+									className="inline-flex items-center justify-center p-2 text-gray-400 bg-gray-900 rounded-md hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-rose-400">
 									<span className="sr-only">Close menu</span>
 									<XIcon className="w-6 h-6" aria-hidden="true" />
 								</Popover.Button>
@@ -77,29 +77,20 @@ export default function Navbar() {
 							<div className="px-2 space-y-1">
 								{navigation.map((item) => (
 								<a key={item.name} href={item.href}
-									className="block px-3 py-2 text-base font-medium text-gray-900 rounded-md hover:bg-gray-50">
+									className="block px-3 py-2 text-base font-medium text-gray-200 rounded-md hover:bg-gray-50">
 									{item.name}
 								</a>
 								))}
 							</div>
 							<div className="px-5 mt-6">
-								<a href="#"
-									className="block w-full px-4 py-3 font-medium text-center text-white rounded-md shadow bg-gradient-to-r from-teal-500 to-cyan-600 hover:from-teal-600 hover:to-cyan-700">
-									Start free trial
+								<a href="/blog/login" className="block w-full px-4 py-3 font-medium text-center text-white rounded-md shadow bg-gradient-to-r from-rose-400 via-fuchsia-500 to-indigo-500">
+									Login
 								</a>
-							</div>
-							<div className="px-5 mt-6">
-								<p className="text-base font-medium text-center text-gray-500">
-									Existing customer?{' '}
-									<a href="#" className="text-gray-900 hover:underline">
-										Login
-									</a>
-								</p>
 							</div>
 						</div>
 					</div>
 				</Popover.Panel>
-			</Transition>*/}
+			</Transition>
 		</Popover>
 	);
 }
